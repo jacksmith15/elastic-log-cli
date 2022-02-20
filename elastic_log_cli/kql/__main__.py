04231d4183aa1f59bdf2fbf26a34b7f5f00a51b7
Jack Smith <jack.je.smith@gmail.com>
2022-02-20 19:46:42 +0000

    KQL parser, transformer and basic test

--- elastic_log_cli/kql/__main__.py
+++ elastic_log_cli/kql/__main__.py
@@ -1,17 +1,20 @@
+import json
 import sys
 
-from elastic_log_cli.kql.parser import get_parser
+from elastic_log_cli.kql.parser import get_parser, parse, transform
 
 
 def main():
     query = sys.argv[1]
     print(f"Got query {query!r}")
-    parser = get_parser()
+    get_parser()
     print("Loaded parser")
-    result = parser.parse(query)
-    print(f"Result: {result.pretty()}")
-    return result
+    tree = parse(query)
+    print(f"Tree:\n{tree.pretty()}")
+    result = transform(tree)
+    print(f"Result:\n{json.dumps(result, indent=2)}")
+    return tree, result
 
 
 if __name__ == "__main__":
-    tree = main()
+    tree, result = main()
