bd2031f644e03d6c3deae19598d3510d806526ea
Jack Smith <jack.je.smith@gmail.com>
2022-02-22 19:20:22 +0000

    Add sub-query support to KQL parser

--- elastic_log_cli/kql/__main__.py
+++ elastic_log_cli/kql/__main__.py
@@ -1,15 +1,15 @@
 import json
 import sys
 
-from elastic_log_cli.kql.parser import get_parser, parse, transform
+from elastic_log_cli.kql.parser import get_parser, transform
 
 
 def main():
     query = sys.argv[1]
     print(f"Got query {query!r}")
-    get_parser()
+    parser = get_parser()
     print("Loaded parser")
-    tree = parse(query)
+    tree = parser.parse(query)
     print(f"Tree:\n{tree.pretty()}")
     result = transform(tree)
     print(f"Result:\n{json.dumps(result, indent=2)}")
